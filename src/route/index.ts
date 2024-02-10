cbf61bce6d6f1a55651e0518560a356b740c9dc7
unknown <fahmi.mellow@gmail.com>
2024-02-10 08:48:22 +0700

    change method auth API

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import authController from "../controllers/authController";
+import * as multer from "multer";
+import userController from "../controllers/userController";
 
 const routes = Router();
-export default routes;
+const upload = multer();
 
 //Auth API
-routes.post("/register", authController.register);
-routes.get("/login", authController.login);
+routes.post("/register", upload.none(), authController.register);
+routes.post("/login", authController.login);
+routes.delete("/logout", authController.logout);
+
+//User API
+routes.get("/search/:username", userController.getUsers);
+
+export default routes;
