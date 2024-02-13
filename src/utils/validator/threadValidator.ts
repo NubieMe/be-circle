412628c8c4222b3d9cac44e8946fad36e76560a2
unknown <fahmi.mellow@gmail.com>
2024-02-13 07:32:43 +0700

    thread validator minor changes

--- src/utils/validator/threadValidator.ts
+++ src/utils/validator/threadValidator.ts
@@ -3,11 +3,11 @@ import joi = require("joi");
 export const createThreadSchema = joi.object({
     content: joi.string().max(160).required(),
     image: joi.string().optional(),
-    created_by: joi.number().required(),
+    author: joi.number().required(),
 });
 
 export const updateThreadSchema = joi.object({
-    content: joi.string().max(160).required(),
+    content: joi.string().max(160).optional(),
     image: joi.string().optional(),
     updated_at: joi.date().default(Date.now()),
 });
