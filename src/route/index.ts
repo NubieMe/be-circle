41bf64a2752a15d92bc7b2ee99679affbefe7145
unknown <fahmi.mellow@gmail.com>
2024-02-14 09:19:31 +0700

    add thread and follow routes

--- src/route/index.ts
+++ src/route/index.ts
@@ -2,16 +2,31 @@ import { Router } from "express";
 import authController from "../controllers/authController";
 import * as multer from "multer";
 import userController from "../controllers/userController";
+import threadController from "../controllers/threadController";
+import authMiddleware from "../middlewares/auth";
+import uploadFile from "../middlewares/uploadFile";
+import followController from "../controllers/followController";
 
 const routes = Router();
 const upload = multer();
 
 //Auth API
-routes.post("/register", upload.none(), authController.register);
+routes.post("/register", authController.register);
 routes.post("/login", authController.login);
 routes.delete("/logout", authController.logout);
 
 //User API
 routes.get("/search/:username", userController.getUsers);
+routes.get("/user/:username", userController.getUser);
+routes.get("/user/current", authMiddleware.auth, userController.getCurrent);
+
+//Thread API
+routes.post("/thread", uploadFile.upload("image"), threadController.createThread);
+routes.get("/thread", threadController.getThreads);
+routes.get("/thread/:id", threadController.getThread);
+
+//Follow API
+routes.patch("/follow", authMiddleware.auth, followController.follow);
+routes.get("/follow", authMiddleware.auth, followController.getFollow);
 
 export default routes;
