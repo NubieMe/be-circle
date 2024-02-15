66fe46ccd48b4b0c718791ed8f8ffdda20ce89eb
unknown <fahmi.mellow@gmail.com>
2024-02-15 08:39:08 +0700

    likeThread feature added

--- src/services/likeService.ts
+++ src/services/likeService.ts
@@ -1,6 +1,14 @@
 import { Repository } from "typeorm";
 import { Like } from "../entities/Like";
+import { AppDataSource } from "../data-source";
 
 export default new (class likeService {
     private readonly likeRepository: Repository<Like> = AppDataSource.getRepository(Like);
+
+    async likeThread(threadId, sessionId) {
+        return this.likeRepository.save({
+            thread: threadId,
+            author: sessionId,
+        });
+    }
 })();
