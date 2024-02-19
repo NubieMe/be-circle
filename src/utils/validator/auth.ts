c9aa07866ef25940fefb00d46c818477ca0d418f
unknown <fahmi.mellow@gmail.com>
2024-02-19 13:32:15 +0700

    add email to registerSchema & minor adjustment

--- src/utils/validator/auth.ts
+++ src/utils/validator/auth.ts
@@ -1,12 +1,13 @@
 import joi = require("joi");
 
 export const registerSchema = joi.object({
-    username: joi.string().max(100).required(),
-    password: joi.string().max(100).required(),
     name: joi.string().max(100).required(),
+    username: joi.string().max(100).required(),
+    email: joi.string().email().max(100).required(),
+    password: joi.string().min(8).max(100).required(),
 });
 
 export const loginSchema = joi.object({
-    username: joi.string().required(),
+    username: joi.string().required().label("Username/Email"),
     password: joi.string().required(),
 });
