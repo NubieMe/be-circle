d15d03c88b1d53929f8916d6282bdb4ca5f97d47
unknown <fahmi.mellow@gmail.com>
2024-02-19 13:25:19 +0700

    rework register & login feature

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,12 +2,11 @@ import { Repository } from "typeorm";
 import { User } from "../entities/User";
 import { AppDataSource } from "../data-source";
 import { validate } from "../utils/validator/validation";
-import { loginSchema, registerSchema } from "../utils/validator/authValidator";
+import { loginSchema, registerSchema } from "../utils/validator/auth";
 import ResponseError from "../error/responseError";
 import * as bcrypt from "bcrypt";
 import * as jwt from "jsonwebtoken";
 import { Request } from "express";
-import followService from "./followService";
 
 export default new (class AuthService {
     private readonly authRepository: Repository<User> = AppDataSource.getRepository(User);
@@ -18,26 +17,24 @@ export default new (class AuthService {
         const chkUser = await this.authRepository.countBy({ username: isValid.username });
         if (chkUser !== 0) throw new ResponseError(400, "Username already exist!");
 
+        const chkEmail = await this.authRepository.countBy({ email: isValid.email });
+        if (chkEmail !== 0) throw new ResponseError(400, "Email already exist!");
+
         const hash = await bcrypt.hash(isValid.password, 10);
 
         await this.authRepository.save({
-            username: isValid.username,
-            password: hash,
             name: isValid.name,
+            username: isValid.username,
+            email: isValid.email,
+            password: hash,
         });
-
         const get = await this.authRepository.findOne({ where: { username: isValid.username } });
-        await followService.createFollow(get.id);
 
         const token = jwt.sign({ id: get.id, username: get.username }, process.env.SECRET_KEY, {
             expiresIn: "7d",
         });
         return {
             message: "Account created",
-            user: {
-                id: get.id,
-                username: get.username,
-            },
             token: token,
         };
     }
@@ -53,20 +50,36 @@ export default new (class AuthService {
                 password: true,
             },
         });
-        if (!chkUser) throw new ResponseError(401, "Username not registered yet!");
-
-        const isEqual = await bcrypt.compare(isValid.password, chkUser.password);
-        if (!isEqual) throw new ResponseError(401, "Username or Password is not correct!");
-
-        const token = jwt.sign({ id: chkUser.id, username: chkUser.username }, process.env.SECRET_KEY, {
-            expiresIn: "7d",
+        const chkEmail = await this.authRepository.findOne({
+            where: { email: isValid.username },
+            select: {
+                id: true,
+                username: true,
+                password: true,
+            },
         });
+        if (!chkUser && !chkEmail) throw new ResponseError(401, "Username/Email not registered yet!");
+
+        let isEqual;
+        let token;
+        if (!chkUser && chkEmail) {
+            isEqual = await bcrypt.compare(isValid.password, chkEmail.password);
+            if (!isEqual) throw new ResponseError(401, "Email or Password is not correct!");
+
+            token = jwt.sign({ id: chkEmail.id, username: chkEmail.username }, process.env.SECRET_KEY, {
+                expiresIn: "7d",
+            });
+        } else if (!chkEmail && chkUser) {
+            isEqual = await bcrypt.compare(isValid.password, chkUser.password);
+            if (!isEqual) throw new ResponseError(401, "Username or Password is not correct!");
+
+            token = jwt.sign({ id: chkUser.id, username: chkUser.username }, process.env.SECRET_KEY, {
+                expiresIn: "7d",
+            });
+        }
+
         return {
             message: "Login success",
-            user: {
-                id: chkUser.id,
-                username: chkUser.username,
-            },
             token: token,
         };
     }
