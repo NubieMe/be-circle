f83e1f7d08022372d31ec581b3e8eba815dc2980
unknown <fahmi.mellow@gmail.com>
2024-02-19 13:36:01 +0700

    like feature completely added

--- src/services/likeService.ts
+++ src/services/likeService.ts
@@ -6,16 +6,74 @@ export default new (class LikeService {
     private readonly likeRepository: Repository<Like> = AppDataSource.getRepository(Like);
 
     async likeThread(threadId, sessionId) {
-        return this.likeRepository.save({
+        const response = await this.likeRepository.save({
             thread: threadId,
             author: sessionId,
         });
+        return {
+            message: "Thread Liked",
+        };
     }
 
     async likeReply(replyId, sessionId) {
-        return this.likeRepository.save({
+        this.likeRepository.save({
             reply: replyId,
             author: sessionId,
         });
+        return {
+            message: "Reply's Liked",
+        };
+    }
+
+    async getLikeThread(threadId, authorId) {
+        const chk = await this.likeRepository
+            .createQueryBuilder("like")
+            .where("like.thread = :thread", { thread: threadId })
+            .andWhere("like.author = :author", { author: authorId })
+            .getOne();
+
+        if (chk) return true;
+        return false;
+    }
+
+    async getLikeReply(replyId, authorId) {
+        const chk = await this.likeRepository
+            .createQueryBuilder("like")
+            .where("like.thread = :thread", { reply: replyId })
+            .andWhere("like.author = :author", { author: authorId })
+            .getOne();
+
+        if (chk) return true;
+        return false;
+    }
+
+    async unlikeThread(id, session) {
+        const getLike = await this.likeRepository
+            .createQueryBuilder("like")
+            .leftJoinAndSelect("like.thread", "thread")
+            .leftJoinAndSelect("like.author", "author")
+            .where("like.thread = :thread", { thread: id })
+            .andWhere("like.author = :author", { author: session })
+            .getOne();
+
+        await this.likeRepository.delete(getLike.id);
+        return {
+            message: "Unliked",
+        };
+    }
+
+    async unlikeReply(id, session) {
+        const getLike = await this.likeRepository
+            .createQueryBuilder("like")
+            .leftJoinAndSelect("like.reply", "reply")
+            .leftJoinAndSelect("like.author", "author")
+            .where("like.reply = :reply", { reply: id })
+            .andWhere("like.author = :author", { author: session })
+            .getOne();
+
+        await this.likeRepository.delete(getLike.id);
+        return {
+            message: "Unliked",
+        };
     }
 })();
