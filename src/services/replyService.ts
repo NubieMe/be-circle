a3dbd0a8ab0ace9d5f22e54bd9143c10b03d9ee8
unknown <fahmi.mellow@gmail.com>
2024-02-20 21:56:03 +0700

    get reply's thread feature added

--- src/services/replyService.ts
+++ src/services/replyService.ts
@@ -2,13 +2,42 @@ import { Repository } from "typeorm";
 import { Reply } from "../entities/Reply";
 import { AppDataSource } from "../data-source";
 import { validate } from "../utils/validator/validation";
-import { createThreadSchema } from "../utils/validator/thread";
 import cloudinary from "../libs/cloudinary";
 import ResponseError from "../error/responseError";
 import { repliesReplySchema, replyThreadSchema } from "../utils/validator/reply";
+import likeService from "./likeService";
 
 export default new (class ReplyService {
     private readonly replyRepository: Repository<Reply> = AppDataSource.getRepository(Reply);
+
+    async getRepliesThread(threadId, userId) {
+        const response = await this.replyRepository
+            .createQueryBuilder("reply")
+            .leftJoinAndSelect("reply.author", "author")
+            .leftJoinAndSelect("reply.likes", "likes")
+            .leftJoinAndSelect("reply.replies", "replies")
+            .where("reply.thread = :thread", { thread: threadId })
+            .getMany();
+        const likes = response.map(async (val) => await likeService.getLikeReply(val.id, userId));
+
+        const replies = [];
+        let i = 0;
+        const len = response.length;
+        for (i; i < len; i++) {
+            replies.push({
+                id: response[i].id,
+                content: response[i].content,
+                image: response[i].image,
+                likes: response[i].likes.length,
+                isLiked: await likes[i],
+                replies: response[i].replies.length,
+                author: response[i].author,
+                created_at: response[i].created_at,
+            });
+        }
+
+        return await Promise.all(replies);
+    }
 
     async replyThread(data) {
         const isValid = validate(replyThreadSchema, data);
@@ -50,43 +79,15 @@ export default new (class ReplyService {
         };
     }
 
-    async repliesReply(data) {
-        const isValid = validate(repliesReplySchema, data);
-        let valid;
+    async deleteReply(id, session) {
+        const chkReply = await this.replyRepository.findOne({ where: { id }, relations: { author: true } });
 
-        if (data.image && data.content) {
-            cloudinary.upload();
-            const upFIle = await cloudinary.destination(isValid.image);
+        if (!chkReply) throw new ResponseError(404, "Reply not found");
+        if (session !== chkReply.author.id) throw new ResponseError(403, "You are not the author of this reply");
 
-            valid = {
-                content: isValid.content,
-                image: upFIle.secure_url,
-                reply: isValid.reply,
-                author: isValid.author,
-            };
-        } else if (!data.image && data.content) {
-            valid = {
-                content: isValid.content,
-                reply: isValid.reply,
-                author: isValid.author,
-            };
-        } else if (data.image && !data.content) {
-            cloudinary.upload();
-            const upFIle = await cloudinary.destination(isValid.image);
-
-            valid = {
-                image: upFIle.secure_url,
-                reply: isValid.reply,
-                author: isValid.author,
-            };
-        } else {
-            throw new ResponseError(400, "Content or Image is required");
-        }
-
-        await this.replyRepository.save(valid);
+        await this.replyRepository.delete(id);
         return {
-            message: "Reply created",
-            data: valid,
+            message: "Reply deleted",
         };
     }
 })();
