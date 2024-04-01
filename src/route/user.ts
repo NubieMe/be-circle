281b6bf5248be59a3ee506b7eda6c287ea8f5d28
NubieMe <fahmi.mellow@gmail.com>
2024-04-01 23:12:34 +0700

    update: change upload any to single

--- src/route/user.ts
+++ src/route/user.ts
@@ -9,8 +9,8 @@ userRoutes.get("/search", authMiddleware.auth, userController.getUsers);
 userRoutes.get("/user/:username", userController.getUser);
 userRoutes.get("/user/me/current", authMiddleware.auth, userController.getCurrent);
 userRoutes.patch("/user/:id", authMiddleware.auth, userController.updateUser);
-userRoutes.patch("/upload/picture/:id", authMiddleware.auth, upload.any(), userController.uploadPicture);
-userRoutes.patch("/upload/cover/:id", authMiddleware.auth, upload.any(), userController.uploadCover);
+userRoutes.patch("/upload/picture/:id", authMiddleware.auth, upload.single("picture"), userController.uploadPicture);
+userRoutes.patch("/upload/cover/:id", authMiddleware.auth, upload.single("cover"), userController.uploadCover);
 userRoutes.delete("/user/:id", authMiddleware.auth, userController.deleteUser);
 userRoutes.get("/suggestion", authMiddleware.auth,userController.suggestion);
 
